424d58b54f5490d41cc0a0d5e9de973b91fba150
Marian <leshchukmarian@gmail.com>
2018-04-25 20:48:21 +0300

    some correction

--- src/app/models/item.model.ts
+++ src/app/models/item.model.ts
@@ -2,13 +2,13 @@ export class Item {
     id: string;
     title: string;
     description: string;
-    prise: number;
+    price: number;
     imageSrc: string;
 
-    constructor(title, description, prise, imageSrc) {
+    constructor(title, description, price, imageSrc) {
          this.title = title;
          this.description = description;
-         this.prise =prise;
+         this.price =price;
          this.imageSrc = imageSrc;
     }
 
